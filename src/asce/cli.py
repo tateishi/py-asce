20b0980d3a3aab68fab329e7a8bcd074a4e58b33
Tateishi Tadatoshi <tateishi@tw.tokai-tv.co.jp>
2021-03-24 11:19:08 +0900

    templates, parametersのディレクトリをカレント起点で検索

--- src/asce/cli.py
+++ src/asce/cli.py
@@ -5,18 +5,18 @@ import asce
 from pathlib import Path
 
 
-PATH0 = Path('/home/thkadmin/wks/python/py-asce/data')
-
-PATH = [
-    PATH0 / 'templates',
+DATA_PATH = Path.cwd() / 'data'
+TEMPLATE_PATH = [
+    DATA_PATH / 'templates',
 ]
+PARAMETER_PATH = DATA_PATH / 'parameters'
 
 
 def main():
     print("Hello, world")
-    print(PATH0)
-    template = asce.load_template(PATH, 'sample.txt')
-    parameter = asce.load_parameter(PATH0 / 'parameters/sample.yml')
+    print(DATA_PATH)
+    template = asce.load_template(TEMPLATE_PATH, 'sample.txt')
+    parameter = asce.load_parameter(PARAMETER_PATH / 'sample.yml')
     print(template.render(parameter))
 
 
