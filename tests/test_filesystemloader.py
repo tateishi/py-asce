240da47b93204ec3dbfacdb196fe4a6dbad5838e
Tateishi Tadatoshi <tateishi@tw.tokai-tv.co.jp>
2021-03-24 18:56:56 +0900

    testのupdate

--- tests/test_filesystemloader.py
+++ tests/test_filesystemloader.py
@@ -2,19 +2,12 @@
 
 
 from asce import load_template
-from pathlib import Path
-
-
-PATH = [
-    Path(__file__).parents[1] / 'data/templates',
-]
 
 
 def test_filesystemloader():
     from datetime import datetime
     now = datetime(2021, 3, 21, 10, 30)
-    print(PATH)
-    file = 'sample.txt'
-    template = load_template(PATH, file)
+    file = 'default.tmpl'
+    template = load_template(file)
     assert (template.render(option='-lA', dir='~', now=now, today="%Y%m%d")
             == 'ls -lA ~ 20210321')
--- tests/test_yamlloader.py
+++ tests/test_yamlloader.py
@@ -2,14 +2,10 @@
 
 
 from asce import load_parameter
-from pathlib import Path
-
-
-PATH = Path(__file__).parents[1] / 'data/parameters'
 
 
 def test_yamlloader():
-    file = PATH / 'sample.yml'
+    file = 'default.yml'
     v = load_parameter(file)
     print(file, v)
     assert (v['option'] == '-lA' and v['dir'] == '~')
